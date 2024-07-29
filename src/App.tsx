5e7f16d79c58c13ee7e121b9a06ad70b0fe11913
samuelea <batonenbois@gmail.com>
2024-07-29 16:25:47 -0400

    calldata

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Box, Button, Text } from '@0xsequence/design-system'
 import { useOpenConnectModal } from '@0xsequence/kit'
 import { useCheckoutModal, CheckoutSettings } from '@0xsequence/kit-checkout'
-import { encodeFunctionData, getContract, Hex } from 'viem'
+import { encodeFunctionData, Hex } from 'viem'
 import { useAccount, useDisconnect, usePublicClient, useWalletClient } from 'wagmi'
 
 import { SALES_CONTRACT_ABI } from './utils/abi'
 
 function App() {
-  const { address, isConnected  } = useAccount()
+  const { address: userAddress, isConnected  } = useAccount()
   const { setOpenConnectModal } = useOpenConnectModal()
   const { disconnect } = useDisconnect()
   const { triggerCheckout } = useCheckoutModal()
@@ -15,33 +15,41 @@ function App() {
   const { data: walletClient } = useWalletClient()
 
   const onClickBuy = () => {
-    if (!publicClient || !walletClient) {
+    if (!publicClient || !walletClient || !userAddress) {
       return
     }
 
     const nftTokenAddress = '0x7204049549d6cdacfb6c8c07a479d80443838927'
     const salesContractAddress = '0x9642c9070fc86838dc6be18f9aad3e7ad301acf6'
+    const currencyAddress = '0x3c499c542cef5e3811e1192ce70d8cc03d5c3359'
 
-    const salesContract = getContract({
-      address: salesContractAddress,
-      abi: SALES_CONTRACT_ABI,
-      client: {
-        public: publicClient,
-        wallet: walletClient
-      }
-    })
+    const currencyPrice = '100000'
+
+    /**
+     * Mint tokens.
+     * @param to Address to mint tokens to.
+     * @param tokenIds Token IDs to mint.
+     * @param amounts Amounts of tokens to mint.
+     * @param data Data to pass if receiver is contract.
+     * @param expectedPaymentToken ERC20 token address to accept payment in. address(0) indicates ETH.
+     * @param maxTotal Maximum amount of payment tokens.
+     * @param proof Merkle proof for allowlist minting.
+     * @notice Sale must be active for all tokens.
+     * @dev tokenIds must be sorted ascending without duplicates.
+     * @dev An empty proof is supplied when no proof is required.
+     */
 
     const calldata = encodeFunctionData({
       abi: SALES_CONTRACT_ABI,
       functionName: 'mint',
       args: [
-        // address to,
-        // uint256[] memory tokenIds,
-        // uint256[] memory amounts,
-        // bytes memory data,
-        // address expectedPaymentToken,
-        // uint256 maxTotal,
-        // bytes32[] calldata proof
+        userAddress as Hex,
+        [BigInt(1)],
+        [BigInt(1)],
+        BigInt(0),
+        currencyAddress as Hex,
+        BigInt(currencyPrice),
+        BigInt(0)
       ]
     })
 
@@ -50,8 +58,8 @@ function App() {
       creditCardCheckout: {
         chainId: 421614,
         contractAddress: salesContractAddress,
-        recipientAddress: address || '',
-        currencyQuantity: '100000',
+        recipientAddress: userAddress || '',
+        currencyQuantity: currencyPrice,
         currencySymbol: 'USDC',
         currencyAddress: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
         currencyDecimals: '6',
@@ -88,7 +96,7 @@ function App() {
 
   return (
     <Box height="full" width="full" gap="2" flexDirection="column" background="backgroundPrimary">
-      <Text color="text100">Address: {address}</Text>
+      <Text color="text100">Address: {userAddress}</Text>
       <Button label="Buy With Credit Card" onClick={onClickBuy} />
       <Button label="Disconnect" onClick={disconnect} />
     </Box>
