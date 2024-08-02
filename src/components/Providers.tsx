95397bcfbb84a4fa741b60e8dd17d350d1d08a2f
samuelea <batonenbois@gmail.com>
2024-08-02 11:27:25 -0400

    switch to waas

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,28 +2,35 @@ import React from 'react'
 
 import { ThemeProvider } from '@0xsequence/design-system'
 import { KitProvider } from '@0xsequence/kit'
-import { getDefaultConnectors } from '@0xsequence/kit-connectors'
+import { getDefaultWaasConnectors } from '@0xsequence/kit-connectors'
 import { KitCheckoutProvider } from '@0xsequence/kit-checkout'
 import { ChainId } from '@0xsequence/network'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Transport, zeroAddress } from 'viem'
+import { Transport } from 'viem'
 import { createConfig, http, WagmiProvider } from 'wagmi'
-import { mainnet, polygon, Chain } from 'wagmi/chains'
-import { sign } from 'viem/accounts'
+import { polygon, Chain } from 'wagmi/chains'
 
 const queryClient = new QueryClient()
 
 export const Providers = ({
   children
 }: { children: React.ReactNode }) => {
-  const projectAccessKey = import.meta.env.VITE_PROJECT_ACCESS_KEY || 'AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI'
+  const projectAccessKey = import.meta.env.VITE_PROJECT_ACCESS_KEY || 'AQAAAAAAADVH8R2AGuQhwQ1y8NaEf1T7PJM'
   const walletConnectProjectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || 'c65a6cb1aa83c4e24500130f23a437d8'
+  const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com";
+  const appleClientId = import.meta.env.VITE_APPLE_CLIENT_ID || 'com.horizon.sequence.waas'
+  const waasConfigKey = import.meta.env.VITE_WAAS_CONFIG_KEY || "eyJwcm9qZWN0SWQiOjEzNjM5LCJycGNTZXJ2ZXIiOiJodHRwczovL3dhYXMuc2VxdWVuY2UuYXBwIn0=";
+  const appleRedirectURI = window.location.origin + window.location.pathname
 
-  const connectors = getDefaultConnectors({
+  const connectors = getDefaultWaasConnectors({
     walletConnectProjectId,
     defaultChainId: ChainId.POLYGON,
     appName: 'demo app',
-    projectAccessKey
+    projectAccessKey,
+    waasConfigKey,
+    googleClientId,
+    appleClientId,
+    appleRedirectURI,
   })
 
   const chains = [polygon] as [Chain, ...Chain[]]
@@ -50,8 +57,8 @@ export const Providers = ({
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
           <KitProvider config={kitConfig}>
-              <KitCheckoutProvider>{children}</KitCheckoutProvider>
-          </KitProvider>
+            <KitCheckoutProvider>{children}</KitCheckoutProvider>
+        </KitProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </ThemeProvider>
